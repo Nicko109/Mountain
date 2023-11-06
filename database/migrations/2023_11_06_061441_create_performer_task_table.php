f8cdc2e50df33b81c5db1fa2268f5a84cf5b3691
nkaragodov@inbox.ru <Nevermore109>
2023-11-06 09:52:21 +0300

    реализованы связи во вью

---
+++ database/migrations/2023_11_06_061441_create_performer_task_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('performer_task', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('performer_id')->index()->constrained('performers');
+            $table->foreignId('task_id')->index()->constrained('tasks');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('performer_tasks');
+    }
+};
